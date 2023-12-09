598062e2aa71c915f0b59cccfddc858d57d12374
Arthur Lagutin <johdoes@example.com>
2023-12-09 23:41:59 +0200

    Stripe integration 29

--- app/Http/Controllers/PasscodeController.php
+++ app/Http/Controllers/PasscodeController.php
@@ -98,7 +98,7 @@ class PasscodeController extends Controller
         // $payment->update(['passcode_displayed' => true]); // Commented out if not needed
     
         // Return the success view with the passcode
-        return view('passcode-success', ['passcode' => $payment->passcode]);
+        return view('threads/passcode-success', ['passcode' => $payment->passcode]);
     }
 
     public function cancel(Request $request)
