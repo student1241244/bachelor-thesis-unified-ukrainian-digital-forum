ea57c8fa4536fcf5b53440cfd6b91950b361788f
Arthur Lagutin <johdoes@example.com>
2023-12-21 01:01:53 +0200

    Http flag

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Symfony\Component\HttpFoundation\Cookie;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -15,4 +16,25 @@ class VerifyCsrfToken extends Middleware
         'admin/*',
         'stripe-webhook'
     ];
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+        $response->headers->setCookie(
+            new Cookie('XSRF-TOKEN',
+                $request->session()->token(),
+                time() + 60 * 120,
+                '/',
+                null,
+                config('session.secure'),
+                false)
+        );
+        return $response;
+    }
 }
