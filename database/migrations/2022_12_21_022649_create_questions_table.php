570619204dfdde019f3c742bb8e66a8c564cdacb
Arthur Lagutin <johdoes@example.com>
2023-12-23 19:50:42 +0200

    Removed migration warning 3

---
+++ database/migrations/2022_12_21_022649_create_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->longText('body');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('category_id'); // Add this line
+    
+            // Foreign key constraint
+            $table->foreign('category_id')->references('id')->on('questions_categories')->onDelete('cascade');
+        });
+    }
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }    
+};
