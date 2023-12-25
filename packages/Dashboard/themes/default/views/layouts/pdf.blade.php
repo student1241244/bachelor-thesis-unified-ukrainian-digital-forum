e8566afcb9c33eff477467713e3e49531ab5b709
Arthur Lagutin <johdoes@example.com>
2023-12-25 21:46:21 +0200

    Testing fixes 23

--- packages/Dashboard/themes/default/views/layouts/pdf.blade.php
+++ packages/Dashboard/themes/default/views/layouts/pdf.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="icon" sizes="16x16" href="/images/favicon.png">
     <link href="/{{ config('tpx_dashboard.public_resources') }}/css/bootstrap.min.css" rel="stylesheet">
     <link href="/{{ config('tpx_dashboard.public_resources') }}/css/font-awesome.min.css" rel="stylesheet">
     <style>
