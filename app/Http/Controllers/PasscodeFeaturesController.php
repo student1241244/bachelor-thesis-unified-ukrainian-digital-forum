484ebf461fea074801b382dbdfb951ac59d887f7
Arthur Lagutin <johdoes@example.com>
2023-12-25 17:36:16 +0200

    Theme switcher 8

--- app/Http/Controllers/PasscodeFeaturesController.php
+++ app/Http/Controllers/PasscodeFeaturesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Payment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PasscodeFeaturesController extends Controller
 {
@@ -14,13 +15,16 @@ class PasscodeFeaturesController extends Controller
         }
         $payment = Payment::where('status', 'completed')->first();
         if ($payment && password_verify($rawPasscode, $payment->passcode)) {
+            Log::error("PASSCODE ACTIVATION", ['1' => '1']);
             $activatedAt = $passcodeSession['activated_at'] ?? null;
+            Log::error("PASSCODE ACTIVATION", ['2' => '2']);
             if (!$activatedAt || now()->diffInMinutes($activatedAt) > 120) {
+                Log::error("PASSCODE ACTIVATION", ['3' => '3']);
                 return false;
             }
             return true;
         }
-
+        Log::error("PASSCODE ACTIVATION", ['4' => '4']);
         return false;
     }
     
